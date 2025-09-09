bb25e528705ca6915865a3ab7cdbacbd700580a3
ringolo <to_be_determined>
2025-09-09 12:20:11 +0200

    initialized postgres service

---
+++ services/postgres/scripts/.init-db/init.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS original_documents(
+    id TEXT PRIMARY KEY,
+    original_file TEXT NOT NULL,
+    import_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    processed_image_file TEXT,
+    raw_ocr TEXT,
+    processed_ocr TEXT
+);
+
+CREATE TABLE IF NOT EXISTS labels(
+    id SERIAL PRIMARY KEY,
+    document_id TEXT,
+    source TEXT,
+    label INT
+);
+
+CREATE TABLE IF NOT EXISTS processed_images(
+    id SERIAL PRIMARY KEY,
+    document_id TEXT,
+    image_file TEXT,
+    processor TEXT,
+    processing_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS raw_texts(
+    id SERIAL PRIMARY KEY,
+    document_id TEXT,
+    processor TEXT,
+    processing_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS processed_texts(
+    id SERIAL PRIMARY KEY,
+    raw_text SERIAL,
+    processor TEXT,
+    processing_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS embeddings(
+    id SERIAL PRIMARY KEY,
+    processed_image SERIAL NOT NULL,
+    processed_text SERIAL NOT NULL,
+    clip_version TEXT NOT NULL,
+    embeddings ARRAY
+);
+
---
+++ services/postgres/scripts/initialize.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker volume dsdc_pgdata
+
+docker pull postgres:latest
