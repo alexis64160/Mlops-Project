8ebfa4e5d7a29a62ea9e0b4ad989dbe39aede0ab
ringolo <to_be_determined>
2025-09-10 18:15:10 +0200

    added logging level to config.yaml

--- dsdc/__init__.py
+++ dsdc/__init__.py
@@ -7,3 +7,12 @@ PROJECT_ROOT = get_project_root()
 CONFIG = load_config_as_namespace(PROJECT_ROOT/"config.yaml", PROJECT_ROOT)
 SECRETS = load_env_file(PROJECT_ROOT/".env.secrets")
 
+import os
+import logging
+
+log_level_str = CONFIG.logging.level
+
+# Convertir le string en niveau de log numérique
+log_level = getattr(logging, log_level_str, logging.WARNING)
+
+logging.basicConfig(level=log_level, format="%(levelname)s: %(message)s")
