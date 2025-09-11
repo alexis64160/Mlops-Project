c4800c3f31ba15a99ebf0c8d13c58cf38f5959a3
ringolo <to_be_determined>
2025-09-11 07:28:18 +0200

    restructured dsdc.utils

--- dsdc/__init__.py
+++ dsdc/__init__.py
@@ -1,7 +1,7 @@
 import yaml
 from pathlib import Path
 
-from dsdc.utils import get_project_root, load_config_as_namespace, load_env_file
+from dsdc.utils.config import get_project_root, load_config_as_namespace, load_env_file
 
 PROJECT_ROOT = get_project_root()
 CONFIG = load_config_as_namespace(PROJECT_ROOT/"config.yaml", PROJECT_ROOT)
