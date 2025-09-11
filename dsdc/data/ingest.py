ec0fd2bff8b9ab1f0ac00399614849e418c0362b
ringolo <to_be_determined>
2025-09-11 21:52:58 +0200

    used label file in ingest

--- dsdc/data/ingest.py
+++ dsdc/data/ingest.py
@@ -7,10 +7,17 @@ from dsdc.db.crud import add_documents_with_labels
 from dsdc.utils.project_files import compute_document_id, compute_storage_path, get_processed_image_path, get_images_files_in_directory
 
 def get_labels(original_file_paths):
-    cdip_ids = [o.parent.name for o in original_file_paths]
-    labels_df = pd.read_csv(CONFIG.paths.rvl_csv)
-    labels = labels_df[labels_df["document_id"].isin(cdip_ids)][["document_id", "label"]].set_index("document_id").to_dict()['label']
-    labels = [labels[d] for d in cdip_ids]
+    labels = []
+    for original_file_path in original_file_paths:
+        file_dir = original_file_path.parent
+        label_file = file_dir/"label.txt"
+        if not label_file.exists():
+            logging.warn(f"no label file found for {original_file_path}")
+            labels.append(None)
+            continue
+        with open(label_file, 'r') as f:
+            label = int(f.read())
+        labels.append(label)
     return labels
 
 def move_files(original_file_paths, storage_file_paths):
@@ -20,7 +27,14 @@ def move_files(original_file_paths, storage_file_paths):
         shutil.move(original_file_path, storage_file_path)
 
 if __name__ == "__main__":
-    original_file_paths = get_images_files_in_directory(CONFIG.paths.to_ingest, absolute=True)
+    # original_file_paths = get_images_files_in_directory(CONFIG.paths.to_ingest)
+    # previous line deprecated because documents can be made of several pages (models dont accept that so far).
+    # consequantly, only first page is kept (in alphabetic order)
+    original_file_paths = [
+        sorted(get_images_files_in_directory(folder))[0]
+        for folder in CONFIG.paths.to_ingest.iterdir()
+        if folder.is_dir()      # avoid .DS_Store
+    ]
     logging.info(f"Preparing to ingest {len(original_file_paths)} documents")
     document_ids = list(map(compute_document_id, original_file_paths))
     storage_file_paths = list(map(compute_storage_path, original_file_paths, document_ids))
@@ -32,4 +46,4 @@ if __name__ == "__main__":
     # # déplacement des fichiers
     move_files(original_file_paths, storage_file_paths)
     for p in CONFIG.paths.to_ingest.iterdir():
-        p.rmdir()
+        shutil.rmtree(p)
