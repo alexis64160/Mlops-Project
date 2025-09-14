057be8394988b0adc7baed78b1ec36663c361d2b
ringolo <to_be_determined>
2025-09-14 07:24:34 +0200

    docker-compose validé pour postgres

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,15 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/utils.sh"
 
 # Find project root
-DSDC_DIR="$(find_project_root ".dsdc_project_root")"
+DSDC_DIR="${DSDC_DIR:-$(find_project_root .dsdc_project_root)}"
 
-$DSDC_DIR/services/postgres/scripts/start.sh
+# generate docker-compose .env file
+env_file=$DSDC_DIR/services/.env
+cat $DSDC_DIR/.env.secrets > $env_file
+
+POSTGRES_DB=$(yq e '.services.postgres.db_name' $DSDC_DIR/config.yaml)
+echo "POSTGRES_DB=$POSTGRES_DB" >> $env_file
+
+cd $DSDC_DIR/services
+docker-compose up -d postgres
+
