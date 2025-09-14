42e1fc46aba5c9d9f32f04cedef0e957a6e47114
ringolo <to_be_determined>
2025-09-14 09:15:08 +0200

    splitted rvl_csv creation and data add tasks

--- services/airflow/dags/mock_new_data.py
+++ services/airflow/dags/mock_new_data.py
@@ -4,14 +4,9 @@ from datetime import datetime
 from dsdc import CONFIG
 from dsdc.data.mock import build_rvl_csv, pull_cdip_images
 
-def add_data():
-    if not CONFIG.paths.rvl_csv.exists():
-        build_rvl_csv()
-    pull_cdip_images()
 
 
 default_args = {
-    # tes arguments, par ex:
     'owner': 'airflow',
     'start_date': datetime(2025, 9, 14),
 }
@@ -19,16 +14,21 @@ default_args = {
 @dag(
     dag_id="dag_mockup_add_data",
     default_args=default_args,
-    schedule=None,  # schedule_interval est déprécié, utiliser schedule
+    schedule="0 * * * *",  # schedule_interval est déprécié, utiliser schedule
     catchup=False,
     description="Génère des données simulées dans data/to_ingest",
     tags=["dsdc", "mock", "add", "data"],
 )
 def dag_mockup_add_data():
+    @task(task_id="ensure_rvl_csv_exists")
+    def ensure_rvl_csv_exists():
+        if not CONFIG.paths.rvl_csv.exists():
+            build_rvl_csv()
 
-    @task(task_id="mock_add_data")
+    @task(task_id="add_data_task")
     def add_data_task():
-        add_data()  # ta fonction que tu as déjà définie
-    add_data_task()
+        pull_cdip_images()
+    
+    ensure_rvl_csv_exists() >> add_data_task()
 
 dag = dag_mockup_add_data()
