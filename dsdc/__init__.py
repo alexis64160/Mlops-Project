8dca7bcf2dfc442653c2231466597c74a75af7bf
ringolo <to_be_determined>
2025-09-17 07:30:15 +0200

    default log level to warning

--- dsdc/__init__.py
+++ dsdc/__init__.py
@@ -1,17 +1,16 @@
-import yaml
-
 from dsdc.utils.config import get_project_root, load_config_as_namespace, load_env_file
-
 PROJECT_ROOT = get_project_root()
-CONFIG = load_config_as_namespace(PROJECT_ROOT/"config.yaml", PROJECT_ROOT)
+secrets_file = PROJECT_ROOT/".env.secrets"
 SECRETS = load_env_file(PROJECT_ROOT/".env.secrets")
+CONFIG = load_config_as_namespace(PROJECT_ROOT/"config.yaml", PROJECT_ROOT)
 
-import os
+
+import yaml
 import logging
 
-log_level_str = CONFIG.logging.level
-
-# Convertir le string en niveau de log numérique
+if hasattr(CONFIG, "logging") and hasattr(CONFIG.logging, "level"):
+    log_level_str = CONFIG.logging.level
+else:
+    log_level_str = "warning"
 log_level = getattr(logging, log_level_str, logging.WARNING)
-
 logging.basicConfig(level=log_level, format="%(levelname)s: %(message)s")
