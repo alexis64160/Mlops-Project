bd165b2e4c677c87a91487793f2ea86baa5e5444
ringolo <to_be_determined>
2025-09-17 07:35:07 +0200

    docker-compose based on airflow template

---
+++ services/airflow/dags/test_services.py
@@ -0,0 +1,44 @@
+import logging
+from dsdc.db import SessionLocal
+from airflow.decorators import dag, task
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 9, 14),
+}
+
+@dag(
+    dag_id="dag_test_services",
+    default_args=default_args,
+    schedule=None,
+    catchup=False,
+    tags=["test", "debug"],
+)
+def test_services():
+        @task(task_id="test_postgres")
+        def test_postgres():
+            expected_tables = {
+                "original_documents",
+                "labels",
+                "processed_images",
+                "raw_texts",
+                "processed_texts",
+                "embeddings"
+            }
+            with SessionLocal() as session:
+                response = session.execute("""
+                    SELECT table_name
+                    FROM information_schema.tables
+                    WHERE table_schema = 'public'
+                """).fetchall()
+            found_tables = set(r[0] for r in response)
+            missing_tables = expected_tables - found_tables
+            if missing_tables == set():
+                logging.info("All expected tables were retrieved")
+            else:
+                error_message = f"Following tables were not retrieved: {missing_tables}"
+                logging.error(error_message)
+                raise ValueError(error_message)
+        test_postgres()
+dag = test_services()
